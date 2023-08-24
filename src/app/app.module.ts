6e6a8cdde32063cf7c173553c04a8ac0072b7727
KiraTeresa <kira.soba@gmx.de>
2023-08-24 12:45:01 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RecipeListComponent} from './components/recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './components/recipes/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './components/recipes/recipe-detail/recipe-detail.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ShoppingListComponent} from './components/shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './components/shopping-list/edit/shopping-list-edit.component';
+import {RecipesComponent} from "./components/recipes/recipes.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +28,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
