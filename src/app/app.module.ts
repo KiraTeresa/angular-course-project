4c39ef8c59eadb00e5a7a6acd5874903ce498f88
KiraTeresa <kira.soba@gmx.de>
2023-09-30 16:14:14 +0200

    Adding auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,29 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
-import {AuthComponent} from "./components/auth/auth.component";
 import {RecipesModule} from "./modules/recipes.module";
 import {ShoppingListModule} from "./modules/shopping-list.module";
 import {SharedModule} from "./modules/shared.module";
 import {CoreModule} from "./modules/core.module";
+import {AuthModule} from "./modules/auth.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/modules/auth.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {AuthComponent} from "../components/auth/auth.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "./shared.module";
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    SharedModule,
+    ReactiveFormsModule,
+    RouterModule.forChild([{path: 'auth', component: AuthComponent}])]
+})
+export class AuthModule {
+}
