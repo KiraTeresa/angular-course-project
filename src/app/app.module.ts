b29f967305fb1c079a7d7c0232c52a5d2cb58750
KiraTeresa <kira.soba@gmx.de>
2023-09-30 16:02:34 +0200

    Add a core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ShoppingListService} from "./services/shopping-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./services/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from "./components/auth/auth.component";
-import {AuthInterceptorService} from "./services/auth-interceptor.service";
 import {RecipesModule} from "./modules/recipes.module";
 import {ShoppingListModule} from "./modules/shopping-list.module";
 import {SharedModule} from "./modules/shared.module";
+import {CoreModule} from "./modules/core.module";
 
 
 @NgModule({
@@ -29,13 +27,9 @@ import {SharedModule} from "./modules/shared.module";
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptorService,
-    multi: true
-  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
